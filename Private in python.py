dcef2c1cee3655c5222af28a11cf86e75dccd3a1
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-05-03 01:02:52 +0600

    Update Private in python.py

--- Private in python.py
+++ Private in python.py
@@ -1,8 +1,11 @@
 class Account:
     def __init__(self, accNo, password):
         self.accNo = accNo
-        self.__password = password          # Here we have used __password instead of password to make it private
+        self.__password = password          # Here we have used "__password" instead of "password" to make it private
+
+    def updatePassword(self):
+        print(self.__password)              # Output: abcde (because it is inside the class)
 
 acc1 = Account('12345', 'abcde')
 print(acc1.accNo)                           # Output: 12345
-print(acc1.__password)                      # Output: error because __password is private and cannot be accessed
+print(acc1.__password)                      # Output: error (because "__password" is private and cannot be accessed outside the class)
