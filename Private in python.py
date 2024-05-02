b9b4a3323773cd4bc8046564542b31f67d5e43a2
Md. Naimul Hasan <120248820+NaimulHasanEmon@users.noreply.github.com>
2024-05-03 00:58:20 +0600

    Create Private in python.py

---
+++ Private in python.py
@@ -0,0 +1,7 @@
+class Account:
+    def __init__(self, accNo, password):
+        self.accNo = accNo
+        self.__password = password          # Here we have used __password instead of password to make it private
+
+acc1 = Account('12345', 'abcde')
+print(acc1.accNo, acc1.__password)          # Output: error because __password is private and cannot be accessed
